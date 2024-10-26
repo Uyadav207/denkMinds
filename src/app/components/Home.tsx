62aa0bb1bcd156b59488df4da6a1f995d6a51254
Utkarsh Yadav <utkarshyadav@Utkarshs-MacBook-Air.local>
2024-10-26 17:36:45 +0200

    #4 updated about section

--- src/app/components/Home.tsx
+++ src/app/components/Home.tsx
@@ -3,12 +3,12 @@ import Image from 'next/image'
 
 const Home: NextPage = () => {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-8">
-      <main className="flex flex-col md:flex-row items-center justify-between w-full max-w-5xl px-8 md:px-20 space-y-8 md:space-y-0 md:space-x-12">
+    <div className="flex flex-col items-center justify-start min-h-screen py-8">
+      <main className="flex flex-col md:flex-row items-center justify-between w-full max-w-5xl px-8 md:px-10 space-y-8 md:space-y-0 md:space-x-12">
         <div className="flex flex-col items-start text-left space-y-6 md:w-2/3">
           <h1 className="text-5xl font-bold leading-tight">
           Evolving Your Ideas with{' '}
-            <span className="text-indigo-600">Innovation</span> and{' '}
+            <span className="text-violet-500">Innovation</span> and{' '}
             <span className="text-green-600">technology</span>
           </h1>
 
@@ -19,13 +19,13 @@ const Home: NextPage = () => {
           <div className="flex space-x-4">
             <a
               href="#"
-              className="px-6 py-3 bg-indigo-600 text-white rounded-lg shadow hover:bg-indigo-700 transition duration-300"
+              className="px-6 py-3 bg-violet-500 text-white rounded-lg shadow hover:bg-violet-700 transition duration-400"
             >
               Learn More
             </a>
             <a
               href="#"
-              className="px-6 py-3 bg-transparent border border-indigo-600 text-indigo-600 rounded-lg shadow hover:bg-indigo-600 hover:text-white transition duration-300"
+              className="px-6 py-3 bg-transparent border border-violet-600 text-violet-600 rounded-lg shadow hover:bg-violet-600 hover:text-white transition duration-300"
             >
               Live Demo
             </a>
@@ -35,10 +35,10 @@ const Home: NextPage = () => {
         {/* Logo Container */}
         <div className="flex flex-col justify-end"> 
             <Image
-                src="/rocket.avif"
+                src="/rocket.gif"
                 alt="Illustration"
-                width={500}
-                height={500}
+                width={600}
+                height={600}
                 className="rounded-md"
             />
         </div>
