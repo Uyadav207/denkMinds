bea4bef1f1cdfab0a064bba9c298ee327c59b1fa
Chaitali Tamboliya <chaitalitamboliya@gmail.com>
2024-10-28 13:09:27 +0100

    Added Footer Component

---
+++ src/app/utils/fontawesome.tsx
@@ -0,0 +1,7 @@
+// lib/fontawesome.js
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fas } from '@fortawesome/free-solid-svg-icons'; // Solid icons
+import { far } from '@fortawesome/free-regular-svg-icons'; // Regular icons
+import { fab } from '@fortawesome/free-brands-svg-icons'; // Brand icons
+
+library.add(fas, far, fab); // Adds all icons to the library
