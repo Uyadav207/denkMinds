287fded25680e1ff4613bd231cdcb3e5c424cd92
Sindhura492 <sindhura7649@gmail.com>
2024-10-29 05:12:09 +0100

    #1 contact-form alignement, style changed, validations changed

--- src/app/components/ContactForm.tsx
+++ src/app/components/ContactForm.tsx
@@ -1,17 +1,28 @@
-import React from 'react';
-import Forms from './forms/Forms';
+import React from "react"
+import Forms from "./forms/Forms"
 
 const ContactForm = () => {
   return (
-    <section id="contactform" className="flex items-center justify-center min-h-screen p-8">
-      <div className="max-w-lg w-full bg-white shadow-lg rounded-lg p-10">
-        <h2 className="text-4xl font-bold text-violet-600 text-center mb-8">Contact Us</h2>
+    <section id="contactform" className="flex min-h-screen">
+      <div className="w-2/5 bg-violet-700 p-10 text-white flex flex-col justify-center">
+        <h2 className="text-4xl font-bold text-white text-center mb-8">
+          Say Hello!
+        </h2>
         <div className="flex justify-center">
           <Forms />
         </div>
       </div>
+      <div className="w-3/5 bg-gray-900 p-10 text-white flex flex-col justify-center">
+        <h2 className="text-4xl font-bold  text-white mb-4">
+          Stop by our <span className="text-violet-400">studio</span> and share
+          your thoughts on your project.
+        </h2>
+        <p className="text-lg text-2xl mb-8 p-4">
+          We always have great coffee....
+        </p>
+      </div>
     </section>
-  );
-};
+  )
+}
 
-export default ContactForm;
+export default ContactForm
