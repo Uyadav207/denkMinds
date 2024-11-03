1dc12ba68dd81d5a78196ebe6114d2a343b901e0
Sindhura492 <sindhura7649@gmail.com>
2024-11-03 14:58:47 +0100

    Ui background changed, logo added

--- src/app/components/Home.tsx
+++ src/app/components/Home.tsx
@@ -4,21 +4,21 @@ const Home: React.FC = () => {
   return (
     <section
       id="home"
-      className="relative bg-noise flex items-center justify-center min-h-screen p-8 overflow-hidden"
+      className="relative flex items-center justify-center min-h-screen p-8 overflow-hidden"
     >
-      <div className="absolute inset-0 bg-background-home"></div>
-      <div className="absolute w-[70rem] h-[70rem] bg-transparent transform border border-dotted border-white shadow-lg -rotate-45 opacity-20"></div>
-      <div className="absolute w-[40rem] h-[40rem] bg-transparent transform border border-dotted border-white shadow-lg -rotate-45 opacity-30"></div>
-      <div className="absolute w-80 h-80 bg-transparent transform border border-dotted border-white shadow-lg -rotate-45 opacity-30"></div>
+      <div className="absolute inset-0"></div>
+      <div className="absolute w-[70rem] h-[70rem] bg-transparent transform border-2 border-dotted border-white shadow-lg -rotate-45 opacity-20 animate-pulse"></div>
+      <div className="absolute w-[40rem] h-[40rem] bg-transparent transform border-2 border-dotted border-white shadow-lg -rotate-45 opacity-30 animate-pulse"></div>
+      <div className="absolute w-80 h-80 bg-transparent transform border-2 border-dotted border-white shadow-lg -rotate-45 opacity-30 animate-pulse"></div>
 
 
 
 
       <main className="relative z-10 flex flex-col md:flex-row items-center justify-center w-full h-full max-w-6xl">
   <img 
-    src="/logo2.webp" 
+    src="/logo.PNG" 
     alt="Logo" 
-    className="w-112 h-auto" 
+    className="w-96 h-auto" 
   />
 </main>
 
