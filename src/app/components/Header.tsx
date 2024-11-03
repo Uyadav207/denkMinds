94a09a0625178bcf8c12ad535ff70ce1c1dd9e77
Sindhura492 <sindhura7649@gmail.com>
2024-11-03 14:04:23 +0100

    #1 issues fixed for navbar

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -6,70 +6,69 @@ import { motion, AnimatePresence } from 'framer-motion'
 
 const Header = () => {
   const [isVisible, setIsVisible] = useState(true)
-  const [hasShadow, setHasShadow] = useState(false)
   const [isMenuOpen, setIsMenuOpen] = useState(false)
-  const [activeLink, setActiveLink] = useState('home') // Track active link
+  const [activeLink, setActiveLink] = useState('home')
 
   useEffect(() => {
     const handleScroll = () => {
       const scrollY = window.scrollY
+      setIsVisible(scrollY === 0 || scrollY > 90)
+    }
 
-      if (scrollY === 0) {
-        setIsVisible(true)
-        setHasShadow(false)
-      } else {
-        setIsVisible(scrollY > 90)
-        setHasShadow(scrollY > 0)
+    const handleResize = () => {
+      if (window.innerWidth > 768) {
+        console.log('hello')
+        setIsMenuOpen(false)
       }
     }
 
     window.addEventListener('scroll', handleScroll)
-    return () => window.removeEventListener('scroll', handleScroll)
+    window.addEventListener('resize', handleResize)
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll)
+      window.removeEventListener('resize', handleResize)
+    }
   }, [])
 
   const toggleMenu = () => {
-    setIsMenuOpen(!isMenuOpen) // Toggle menu visibility
+    setIsMenuOpen(!isMenuOpen) 
   }
 
-  // Function to handle smooth scrolling
   const handleScroll = (
     event: React.MouseEvent<HTMLAnchorElement, MouseEvent>,
     targetId: string,
   ) => {
     event.preventDefault()
     const targetElement = document.getElementById(targetId)
-
+    
     if (targetElement) {
       targetElement.scrollIntoView({ behavior: 'smooth', block: 'start' })
-      setActiveLink(targetId) // Set active link when clicked
-    }
-    if (isMenuOpen) {
-      toggleMenu() // Close mobile menu after navigation
+      setActiveLink(targetId) 
+      if (isMenuOpen) toggleMenu() 
     }
   }
 
   return (
     <>
       <header
-        className={`fixed top-0 left-0 w-full z-50 transition-transform duration-300 ease-in-out header ${isVisible
-            ? 'transform translate-y-0'
-            : 'transform -translate-y-full'
-          }`}
+        className={`fixed top-0 left-0 w-full z-50 transition-transform duration-300 ease-in-out ${
+          isVisible ? 'transform translate-y-0' : 'transform -translate-y-full'
+        }`}
       >
-        <div className="container mx-auto flex justify-between space-x-7 items-center p-4">
-          <div className="flex items-center space-x-2 md:flex">
-            <p className="text-white font-bold text-2xl">denkMinds</p>
-          </div>
+        <div className="container mx-auto flex justify-between items-center p-4">
+          <p className="text-white font-bold text-2xl">denkMinds</p>
 
           <div className="hidden md:flex items-center space-x-6">
-            <nav className="flex space-x-7 bg-white bg-opacity-20 backdrop-blur shadow-lg p-2 pr-4 pl-4 rounded-3xl border-black">
+            <nav className="flex space-x-7 bg-white bg-opacity-20 backdrop-blur shadow-lg p-2 rounded-3xl border-black">
               {['home', 'mission', 'team', 'contactform'].map((section) => (
                 <a
                   key={section}
                   href={`#${section}`}
                   onClick={(e) => handleScroll(e, section)}
-                  className={`transition
-                              ${activeLink === section ? ' text-black bg-white rounded-full p-1 text-sm pr-2 pl-2' : ' text-white'}`}
+                  className={`transition ${
+                    activeLink === section ? 'text-black bg-white rounded-full p-1 text-sm' : 'text-white'
+                  }`}
                 >
                   {section.charAt(0).toUpperCase() + section.slice(1)}
                 </a>
@@ -77,7 +76,7 @@ const Header = () => {
             </nav>
           </div>
 
-          <div className="md:hidden right-0 top-0">
+          <div className="md:hidden">
             <button onClick={toggleMenu} aria-label="Toggle menu">
               <svg
                 className="w-8 h-8 text-black"
@@ -95,30 +94,31 @@ const Header = () => {
               </svg>
             </button>
           </div>
-          <div>
-            <button disabled className="bg-transparent text-white font-bold py-2 px-4 rounded-3xl transition duration-200 hover:bg-tarnsparent border-2 border-violet-900">
-              Launch App
-            </button>
-          </div>
+          <button
+            disabled
+            className="bg-transparent text-white font-bold py-2 px-4 rounded-3xl transition duration-200 hover:bg-transparent border-2 border-violet-900 opacity-70 cursor-not-allowed"
+          >
+            Launch App
+          </button>
         </div>
 
         <AnimatePresence>
           {isMenuOpen && (
             <motion.div
-              className="fixed inset-0 bg-gray bg-opacity-50 z-40"
+              className="fixed inset-0 bg-gray-900 bg-opacity-50 z-40"
               onClick={toggleMenu}
               initial={{ opacity: 0 }}
               animate={{ opacity: 1 }}
               exit={{ opacity: 0 }}
             >
               <motion.div
-                className="fixed right-0 top-0 w-64  h-full shadow-lg header z-50 backdrop-filter backdrop-blur-lg"
+                className="fixed top-0 left-0 w-full h-full shadow-lg bg-transparent z-50 backdrop-filter backdrop-blur-lg"
                 initial={{ x: '100%' }}
                 animate={{ x: '0%' }}
                 exit={{ x: '100%' }}
                 transition={{ stiffness: 100 }}
               >
-                <div className="flex justify-between items-center p-4 top-0 right-0">
+                <div className="flex justify-between items-center p-4">
                   <button
                     onClick={toggleMenu}
                     aria-label="Close menu"
@@ -140,7 +140,7 @@ const Header = () => {
                     </svg>
                   </button>
                 </div>
-                <nav className="flex flex-col p-4 space-y-4 header backdrop-filter min-h-screen">
+                <nav className="flex flex-col p-4 space-y-4 min-h-screen">
                   {['home', 'mission', 'team', 'contactform'].map((section) => (
                     <Link
                       key={section}
@@ -152,11 +152,12 @@ const Header = () => {
                       {section.charAt(0).toUpperCase() + section.slice(1)}
                     </Link>
                   ))}
-                  <div>
-                    <button className="bg-blue-700 text-white font-bold py-2 px-4 rounded-3xl transition duration-200 hover:bg-blue-600">
-                      Launch App
-                    </button>
-                  </div>
+                  <button
+                    className="w-full bg-blue-700 text-white font-bold py-2 rounded-3xl transition duration-200 opacity-70 cursor-not-allowed"
+                    disabled
+                  >
+                    Launch App
+                  </button>
                 </nav>
               </motion.div>
             </motion.div>
