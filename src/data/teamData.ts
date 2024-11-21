05117d67b75bc4630b9fd084c514c446f0755cf5
Shweta Kadam <shweta.kadam@s2023.tu-chemnitz.de>
2024-11-22 00:50:31 +0100

    Image issue solved

--- src/data/teamData.ts
+++ src/data/teamData.ts
@@ -2,7 +2,7 @@ export const teamMembers = [
   {
     name: 'Utkarsh Yadav',
     role: 'Project Manager/Co-Founder',
-    image: '/TeamImages/utkarsh-yadav.jpeg',
+    image: '/TeamImages/utkarsh_yadav.jpeg',
     linkedinUrl: 'https://www.linkedin.com/in/yadavutkarsh',
     githubUrl: 'https://github.com/Uyadav207',
     portfolioUrl: 'https://utkarshyadav.vercel.app/',
@@ -10,7 +10,7 @@ export const teamMembers = [
   {
     name: 'Shweta Kadam',
     role: 'QA-Automation/Co-Founder',
-    image: '/TeamImages/shweta-kadam.jpg',
+    image: '/TeamImages/shweta_kadam.jpeg',
     linkedinUrl: 'https://www.linkedin.com/in/shweta-kadam-726875193/',
     githubUrl: 'https://github.com/kadamshweta11',
     portfolioUrl: '',
@@ -18,15 +18,15 @@ export const teamMembers = [
   {
     name: 'Sindhura Shivaprasad',
     role: 'AI Engineer/Co-Founder',
-    image: '/TeamImages/sindhura-shivaprasad.jpeg',
+    image: '/TeamImages/sindhura_shivaprasad.jpeg',
     linkedinUrl: 'https://www.linkedin.com/in/sindhura-s-9a55731b4/',
     githubUrl: 'https://github.com/Sindhura492',
     portfolioUrl: '',
   },
   {
     name: 'Chaitali Tamboliya',
-    role: 'Data Science and digital Marketing/Co-Founder',
-    image: '/TeamImages/chaitali-tamboliya.jpg',
+    role: 'Data Science and Digital Marketing/Co-Founder',
+    image: '/TeamImages/chaitali_tamboliya.jpeg',
     linkedinUrl: 'https://www.linkedin.com/in/chaitali-tamboliya/',
     githubUrl: 'https://github.com/chaitali-1805/',
     portfolioUrl: '',
@@ -34,13 +34,13 @@ export const teamMembers = [
   {
     name: 'Nithin Valiyaveedu',
     role: 'CTO/Co-Founder',
-    image: '/TeamImages/nithin-valiyaveedu.jpg',
+    image: '/TeamImages/nithin_valiyaveedu.jpeg',
     linkedinUrl: 'https://www.linkedin.com/in/nithin-valiyaveedu/',
     githubUrl: 'https://github.com/Nithin-Valiyaveedu',
     portfolioUrl: 'https://3d-portfolio-steel-sigma.vercel.app/',
   },
   {
-    name: 'Maria George',
+    name: 'Mariya George',
     role: 'UI-UX/Co-Founder',
     image: '/TeamImages/mariya_george.jpeg',
     linkedinUrl: 'http://www.linkedin.com/in/mariya-george-926193224',
