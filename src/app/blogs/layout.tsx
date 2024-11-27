d6dbbd814e92e6b67041a5c8f5bf02c4c2f4bbed
Shweta Kadam <shweta.kadam@s2023.tu-chemnitz.de>
2024-11-27 13:48:09 +0100

    #71 responsiveness and backbutton fixed

--- src/app/blogs/layout.tsx
+++ src/app/blogs/layout.tsx
@@ -1,6 +1,6 @@
-import Link from "next/link";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faArrowLeft } from "@fortawesome/free-solid-svg-icons";
+
+import BackButton from "./BackButton";
+
 
 export default function RootLayout({
   children,
@@ -9,16 +9,9 @@ export default function RootLayout({
 }>) {
   return (
     <article className="min-h-screen max-w-3xl mx-auto py-12 px-2 sm:px-8 lg:px-12 text-whit rounded-lg shadow-md">
-      {/* Back Button */}
-      <Link
-        className="flex items-center space-x-2 mb-8 text-blue-400 hover:text-blue-500 font-bold"
-        href={"/"}
-      >
-        <FontAwesomeIcon icon={faArrowLeft} className="h-4 w-4 sm:h-5 sm:w-5" />
-        <span>Back</span>
-      </Link>
+     
+     <BackButton/>
 
-      {/* Blog Content */}
       <section className="prose prose-invert md:prose-lg lg:prose-xl mx-auto">
         {children}
       </section>
