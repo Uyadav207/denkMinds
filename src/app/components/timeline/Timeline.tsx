b9b6886b2ee6c1a572ff27c95b389427f040f465
Nithin Valiyaveedu <nithinvaliya@gmail.com>
2024-12-30 18:13:32 +0100

    initialized template for milestone

---
+++ src/app/components/timeline/Timeline.tsx
@@ -0,0 +1,85 @@
+import React from "react";
+
+import "./Timeline.css";
+
+const Timeline = () => {
+	return (
+		<>
+			{/* Main timeline container */}
+			<div
+				id="timeline"
+				className=" w-full flex flex-col justify-center items-center min-h-screen"
+			>
+				<div className="timeline-container">
+					{/* Individual timeline point */}
+					<div className="timeline-point">
+						<p>1</p>
+						<div className="popup">
+							<div className="popup-number">1</div>
+							<div className="popup-details">
+								<div className="popup-title">Milestone 1</div>
+								Lorem ipsum dolor sit amet conse adipisicing elit. Dicta vitae
+								voluptates id hic recusandae omnis cupiditate odit quis,
+								assumenda optio?
+							</div>
+						</div>
+					</div>
+					{/* Individual timeline point */}
+					<div className="timeline-point">
+						<p>2</p>
+						<div className="popup">
+							<div className="popup-number">2</div>
+							<div className="popup-details">
+								<div className="popup-title">Milestone 1</div>
+								Lorem ipsum dolor sit amet conse adipisicing elit. Dicta vitae
+								voluptates id hic recusandae omnis cupiditate odit quis,
+								assumenda optio?
+							</div>
+						</div>
+					</div>
+					{/* Individual timeline point */}
+					<div className="timeline-point">
+						<p>3</p>
+						<div className="popup">
+							<div className="popup-number">3</div>
+							<div className="popup-details">
+								<div className="popup-title">Milestone 3</div>
+								Lorem ipsum dolor sit amet conse adipisicing elit. Dicta vitae
+								voluptates id hic recusandae omnis cupiditate odit quis,
+								assumenda optio?
+							</div>
+						</div>
+					</div>
+					{/* Individual timeline point */}
+					<div className="timeline-point">
+						<p>4</p>
+						<div className="popup">
+							<div className="popup-number">4</div>
+							<div className="popup-details">
+								<div className="popup-title">Milestone 4</div>
+								Lorem ipsum dolor sit amet conse adipisicing elit. Dicta vitae
+								voluptates id hic recusandae omnis cupiditate odit quis,
+								assumenda optio?
+							</div>
+						</div>
+					</div>
+					{/* Individual timeline point */}
+					<div className="timeline-point">
+						<p>5</p>
+						<div className="popup">
+							<div className="popup-number">5</div>
+							<div className="popup-details">
+								<div className="popup-title">Milestone 5</div>
+								Lorem ipsum dolor sit amet conse adipisicing elit. Dicta vitae
+								voluptates id hic recusandae omnis cupiditate odit quis,
+								assumenda optio?
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</>
+	);
+};
+
+export default Timeline;
