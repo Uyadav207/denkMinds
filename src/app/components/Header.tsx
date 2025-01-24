c06cf453af6e14a0110faf20439d839b8be6b416
Shweta Kadam <shweta.kadam@s2023.tu-chemnitz.de>
2025-01-24 11:05:31 +0100

    Button disabled

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -131,13 +131,14 @@ const Header = () => {
 					)}
 
 					<div className="flex items-center space-x-4">
-						<button
-							type="button"
-							onClick={() => window.open("https://mira-denkminds.vercel.app/login", "_blank")}
-							className="bg-[#080115] hidden md:block text-white font-bold py-2 px-4 rounded-3xl transition duration-200 hover:bg-transparent border-2 border-violet-900 cursor-not-allowebg-[#080115] hidden md:block text-white font-bold py-2 px-4 rounded-3xl transition duration-200 hover:bg-transparent border-2 border-violet-900"
-						>
-							Launch App
-						</button>
+					<button
+	type="button"
+	onClick={() => {}}
+	disabled
+	className="bg-[#080115] hidden md:block text-white font-bold py-2 px-4 rounded-3xl transition duration-200 border-2 border-violet-900 cursor-not-allowed"
+>
+	Launch App
+</button>
 
 						<button
 							type="button"
